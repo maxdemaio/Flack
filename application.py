6e9ca20e92a5e23a4d2d5e52676b2ce6dcd92c02
Maxwell DeMaio <maxdman321@gmail.com>
2020-06-20 23:29:16 -0400

    Organized files and started working on adding new channels

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+
 # Chat posts (Limit to 100)
 posts = []
 myRooms = ["Lounge", "Chess", "News", "Coding"]
@@ -43,6 +44,15 @@ def posts():
 
     # Return list of posts
     return jsonify(data)
+
+@app.route("/newChannel", methods=["POST"])
+def newChannel():
+    # TODO
+    # Make it so users can create rooms
+    # Have a route that when the "+" button is clicked, create a new text file in channels/
+    # Client side have a new paragraph to display that the room is available to join
+    # check that name doesn't already exist
+    return
 
 
 ## Socket IO event bucket handlers
